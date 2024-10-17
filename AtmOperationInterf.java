253cc1f4f19536377238c0691ee055ece4381ac7
meenatchi21 <117801243+meenatchi21@users.noreply.github.com>
2024-10-17 17:49:57 +0530

    Add files via upload

---
+++ AtmOperationInterf.java
@@ -0,0 +1,8 @@
+package project1;
+
+public interface AtmOperationInterf {
+	public void ViewBalance();
+	public void withdrawAmount(double withdrawAmount);
+	public void depositAmount(double depositAmount);
+	public void viewMiniStatement();
+}
